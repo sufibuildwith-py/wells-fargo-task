147ec80fff501930774301bd79b27cda96fdb048
Y-Jacob Mlo <Jacobmlo.2002@gmail.com>
2026-04-10 10:03:24 -0400

    data models

---
+++ src/main/java/com/wellsfargo/counselor/entity/Security.java
@@ -0,0 +1,104 @@
+package com.wellsfargo.counselor.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.util.Date;
+import java.math.BigDecimal;
+
+
+@Entity
+public class Security {
+
+    @Id
+    @GeneratedValue()
+    private long securityId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private Date purchaseDate;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal purchasePrice;
+
+    @Column(nullable = false, precision = 15, scale = 4)
+    private BigDecimal quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "portfolio_id")
+    private Portfolio portfolio;
+
+    protected Security() {
+
+    }
+
+    public Security(String name, Date purchaseDate, String category, BigDecimal purchasePrice, BigDecimal quantity, Portfolio portfolio) {
+        this.name = name;
+        this.purchaseDate = purchaseDate;
+        this.category = category;
+        this.purchasePrice = purchasePrice;
+        this.quantity = quantity
+        this.portfolio = portfolio
+    }
+
+    public long getSecurityId() {
+        return securityId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(Date purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public BigDecimal getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(BigDecimal purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public BigDecimal getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(BigDecimal quantity) {
+        this.quantity = quantity;
+    }
+
+    public Portfolio getPortfolio() {
+        return portfolio;
+    }
+
+    public void setPortfolio(Portfolio portfolio) {
+        this.portfolio = portfolio;
+    }
+}
